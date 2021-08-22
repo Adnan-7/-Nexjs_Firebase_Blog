4e3ad14fdd542622b94f510f81c8da4317dc6200
Adnan Khan <adnan7.zain@gmail.com>
2021-08-22 16:23:58 +0500

    get blogs from firestore and display on home page

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,66 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import {db} from '../firebase'
+import Link from 'next/link'
 
-export default function Home() {
+export default function Home({allBlogs}) {
   return (
-    <div className={styles.container}>
-    <h1>HomePage</h1>
+    <div className='container'>
+
+      {allBlogs.map((blog)=>{
+        return(
+          <div className="card" key={blog.id}>
+          <div className="card-image">
+            <img src={blog.imageUrl} />
+            <span className="card-title">{blog.title}</span>
+          </div>
+          <div className="card-content">
+            <p>{blog.body}</p>
+          </div>
+          <div className="card-action">
+           <Link href={`/blog/${blog.id}`}><a>Read More</a></Link> 
+          </div>
+        </div>
+        )
+      })}
+
+      <style jsx>
+        {
+          
+          `
+           
+          p{
+            display:-webkit-box;
+            overflow: hidden;
+            -webkit-line-clamp: 2;
+            -webkit-box-orient:vertical;
+          }
+          `
+        }
+
+      </style>
          
-    </div>
+    </div>  
   )
+}
+
+
+export const getServerSideProps= async (context) =>{
+
+const querySnap =  await db.collection('blogs').orderBy('createdAt','desc').get()
+
+const allBlogs = querySnap.docs.map((docSnap)=>{
+  return{
+    ...docSnap.data(),
+    createdAt:docSnap.data().createdAt.toMillis(),
+    id:docSnap.id
+
+  }
+})
+
+  return {
+    props:{allBlogs}
+  }
+  
 }
